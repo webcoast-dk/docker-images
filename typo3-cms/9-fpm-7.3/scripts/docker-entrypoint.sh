0362c695de97291a6c9a145133fd89027b1df7ae
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2019-08-20 22:01:53 +0200

    [FEATURE] PHP FPM 7.3 for TYPO3 CMS 9 (Debian Stretch)

---
+++ typo3-cms/9-fpm-7.3/scripts/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace environment variables in SSMTP configuration
+(echo "cat <<EOF"; cat /etc/ssmtp/ssmtp.conf.template; echo EOF) | sh > /etc/ssmtp/ssmtp.conf
+
+# Exec the real command
+exec docker-php-entrypoint "$@"
