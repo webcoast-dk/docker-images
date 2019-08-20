61c56551fbeb2af477844263412d3945e1525e36
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2019-08-20 22:05:49 +0200

    [TASK] Improve `build.sh`
    * use remote build cache by default and allow using local build cache
      based on command line argument
    * fix warnings with `find` command

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ declare -a images;
 build=false
 push=false
 verbose=false
+useLocalCache=false
 
 if [ "$#" -gt 0 ]; then
     while [ "$#" -gt 0 ]; do
@@ -18,6 +19,9 @@ if [ "$#" -gt 0 ]; then
                 ;;
             "-v"|"--verbose")
                 verbose=true
+                shift;;
+            "-l"|"--local-cache")
+                useLocalCache=true
                 shift;;
             *)
                 if [ ${build} == false -a ${push} == false ]; then
@@ -37,7 +41,7 @@ fi
 # If not folders are given, use all
 if [ ${#images} -eq 0 ]; then
     for firstLevelFolder in `find . -type d -not -iname '.*' -maxdepth 1`; do
-        for secondLevelFolder in `find $firstLevelFolder -type d -not -iname '.*' -mindepth 1 -maxdepth 1`; do
+        for secondLevelFolder in `find $firstLevelFolder -mindepth 1 -maxdepth 1 -type d -not -iname '.*'`; do
             images[${#images[*]}]="$secondLevelFolder";
         done
     done
@@ -48,11 +52,14 @@ for path in ${images[*]}; do
         repository=${BASH_REMATCH[1]}
         tag=${BASH_REMATCH[2]}
         if ${build}; then
+            if ! ${useLocalCache}; then
+                cacheFrom=" --cache-from webcoastdk/$repository:$tag"
+            fi
             if ${verbose}; then
-                docker build --pull -t webcoastdk/"$repository":"$tag" "$repository/$tag"
+                docker build --pull ${cacheFrom} -t webcoastdk/"$repository":"$tag" "$repository/$tag"
             else
                 echo -n "Building $repository:$tag... "
-                output=$(docker build --pull -t webcoastdk/"$repository":"$tag" "$repository/$tag" 2>&1)
+                output=$(docker build --pull ${cacheFrom} -t webcoastdk/"$repository":"$tag" "$repository/$tag" 2>&1)
                 result=$?
                 if [ ${result} -gt 0 ]; then
                     echo "failed"
@@ -68,7 +75,7 @@ for path in ${images[*]}; do
             if [ ${verbose} == true ]; then
                 docker push webcoastdk/"$repository":"$tag"
             else
-                echo -n "Pushing $repository:$tag..."
+                echo -n "Pushing $repository:$tag... "
                 output=$(docker push webcoastdk/"$repository":"$tag" 2>&1)
                 result=$?
                 if [ ${result} -gt 0 ]; then
